e51690899ee7da0016c59754c6ab58d8cfd0a8a9
Zaph-x <35431127+Zaph-x@users.noreply.github.com>
2020-10-09 09:58:09 +0200

    Checking for invalid import
    Signed-off-by: Zaph-x <35431127+Zaph-x@users.noreply.github.com>

--- tests/test_preprocessor.py
+++ tests/test_preprocessor.py
@@ -1,11 +1,11 @@
 
-from preprocessor import PreProcessor
+import preprocessor.PreProcessor as pr
 import spacy
 
 class Test:
     def test_preprocessor_returns_correct_words(self):
         data = "Motivation er et begreb, der refererer til en proces"
-        result = PreProcessor.remove_stop_words(data)
+        result = pr.remove_stop_words(data)
         assert ['Motivation','begreb', 'refererer', 'proces'] == result
         assert 'er' not in result
     
@@ -22,7 +22,7 @@ class Test:
     selv brudt sammen? Af hvad Art er
     dne Styrke, der holder det oppe?
     """
-        result = PreProcessor.convert_to_modern_danish(data, spacy.load('da_core_news_lg'))
+        result = pr.convert_to_modern_danish(data, spacy.load('da_core_news_lg'))
         assert "formål" in result and "formaal" not in result
         assert "rejsen" in result and "reisen" not in result
         assert "egenskaber" in result and "Egenskaber" not in result
