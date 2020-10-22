31c928bd894a004241188407924de62de99d43e7
AxialDark <alex.klitgaard@gmail.com>
2020-10-22 14:41:59 +0200

    - Added error handling to the file loader, to keep it running on errors

--- loader/FileLoader.py
+++ loader/FileLoader.py
@@ -2,6 +2,8 @@ from watcher.FileWatcher import FileWatcher
 from watchdog.events import FileSystemEventHandler
 from knox_source_data_io.io_handler import IOHandler, Generator, Wrapper
 from loader.JsonWrapper import Publication
+from environment.EnvironmentConstants import EnvironmentVariables as ev
+import os, shutil
 
 class Handler(FileSystemEventHandler):
     """
@@ -19,8 +21,18 @@ class Handler(FileSystemEventHandler):
         elif event.event_type == 'modified':
             if event.src_path.endswith('.json'):    
                 # TODO Fix so that this is a single method call
-                publication: Publication = load_json(event.src_path)
-                print(publication)
+                try:
+                    publication: Publication = load_json(event.src_path)
+                    print(publication)
+                    move_to_folder(event.src_path, ev().get_value(ev().OUTPUT_DIRECTORY), get_file_name_from_path(event.src_path))
+                except FileExistsError as e:
+                    pass # Intentional pass
+                except Exception as e:
+                    if os.path.exists(event.src_path):
+                        print("Move file <" + event.src_path + "> with exception: " + e.__str__())
+                        move_to_folder(event.src_path, ev().get_value(ev().ERROR_DIRECTORY), get_file_name_from_path(event.src_path))
+                    else:
+                        print("Did not find file with path <" + event.src_path + ">, it was likely moved just before...")
 
 def load_json(json_path: str) -> Publication:
     """
@@ -46,3 +58,9 @@ def start_watch_directory(directory: str):
     """
     file_watcher = FileWatcher(directory)
     file_watcher.run(Handler())
+
+def move_to_folder(src_path: str, dest_folder: str, dest_file_name: str) -> str:
+    shutil.move(src=src_path, dst=f'{dest_folder}{dest_file_name}')
+
+def get_file_name_from_path(path: str) -> str:
+    return os.path.split(path)[-1]
--- loader/__init__.py
+++ loader/__init__.py
@@ -2,7 +2,7 @@ import knox_util, os
 import shutil
 
 from environment.EnvironmentConstants import EnvironmentVariables as ev
-from loader.FileLoader import load_json
+from loader.FileLoader import load_json, move_to_folder
 
 def process_existing(path: str) -> None:
     """
@@ -27,10 +27,12 @@ def process_existing(path: str) -> None:
             # TODO create separate function to handle this
             news = load_json(path)
 
-            shutil.move(src=path, dst=f'{ev().get_value(ev().OUTPUT_DIRECTORY)}{split_path[-1]}')
+            move_to_folder(path, ev().get_value(ev().OUTPUT_DIRECTORY), split_path[-1])
+            #shutil.move(src=path, dst=f'{ev().get_value(ev().OUTPUT_DIRECTORY)}{split_path[-1]}')
         except:
-            shutil.move(src=path, dst=f'{ev().get_value(ev().ERROR_DIRECTORY)}{split_path[-1]}')
+            move_to_folder(path, ev().get_value(ev().ERROR_DIRECTORY), split_path[-1])
+            #shutil.move(src=path, dst=f'{ev().get_value(ev().ERROR_DIRECTORY)}{split_path[-1]}')
 
 
     # Simulated finished
-    
+
