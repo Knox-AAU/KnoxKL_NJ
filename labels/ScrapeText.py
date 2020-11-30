f17564a0d8a07e050a6ece4cbee09de39b066800
MKKlausen <mathias.karstoft.klausen@gmail.com>
2020-11-30 09:09:42 +0100

    Model training

--- labels/ScrapeText.py
+++ labels/ScrapeText.py
@@ -93,12 +93,3 @@ def find_matches(search_string: str, search_text: str, last_matches: dict) -> li
         next_index = search_text.find(search_string, last_search)
     
     return index_list
-    
-
-    # train_data = [
-    #    ('Uber blew through $1 million a week', [(0, 4, 'ORG')]),
-    #    ('Android Pay expands to Canada', [(0, 11, 'PRODUCT'), (23, 30, 'GPE')]),
-    #    ('Spotify steps up Asia expansion', [(0, 8, 'ORG'), (17, 21, 'LOC')]),
-    #    ('Google Maps launches location sharing', [(0, 11, 'PRODUCT')]),
-    #    ('Google rebrands its business apps', [(0, 6, 'ORG')]),
-    #    ('look what i found on google! 😂', [(21, 27, 'PRODUCT')])]
---
+++ labels/TrainModel.py
@@ -0,0 +1,94 @@
+from __future__ import unicode_literals, print_function
+
+import plac
+import random
+import warnings
+from pathlib import Path
+import spacy
+from spacy.util import minibatch, compounding
+import labels.output.training_data as train_file
+import labels.output.medical_data as medical_file
+
+# training data
+TRAIN_DATA = medical_file.TRAIN_DATA
+
+
+
+@plac.annotations(
+    model=("Model name. Defaults to blank 'en' model.", "option", "m", str),
+    new_model_name=("New model name for model meta.", "option", "nm", str),
+    output_dir=("Optional output directory", "option", "o", Path),
+    n_iter=("Number of training iterations", "option", "n", int),
+)
+def main(model="C:/Users/skyri/Desktop/Software/Models/Good-Date-backup", new_model_name="da_core_news_date_medical_lg", output_dir="C:/Users/skyri/Desktop/Software/Models", n_iter=50):
+    """Set up the pipeline and entity recognizer, and train the new entity."""
+    random.seed(0)
+    if model is not None:
+        nlp = spacy.load(model)  # load existing spaCy model
+        print("Loaded model '%s'" % model)
+    else:
+        nlp = spacy.blank("en")  # create blank Language class
+        print("Created blank 'en' model")
+    # Add entity recognizer to model if it's not in the pipeline
+    # nlp.create_pipe works for built-ins that are registered with spaCy
+    if "ner" not in nlp.pipe_names:
+        ner = nlp.create_pipe("ner")
+        nlp.add_pipe(ner)
+    # otherwise, get it, so we can add labels to it
+    else:
+        ner = nlp.get_pipe("ner")
+
+    ner.add_label('MEDICAL')  # add new entity label to entity recognizer
+    # Adding extraneous labels shouldn't mess anything up
+    if model is None:
+        optimizer = nlp.begin_training()
+    else:
+        optimizer = nlp.resume_training()
+    move_names = list(ner.move_names)
+    # get names of other pipes to disable them during training
+    pipe_exceptions = ["ner", "trf_wordpiecer", "trf_tok2vec"]
+    other_pipes = [pipe for pipe in nlp.pipe_names if pipe not in pipe_exceptions]
+    # only train NER
+    with nlp.disable_pipes(*other_pipes), warnings.catch_warnings():
+        # show warnings for misaligned entity spans once
+        warnings.filterwarnings("once", category=UserWarning, module='spacy')
+
+        sizes = compounding(1.0, 4.0, 1.001)
+        # batch up the examples using spaCy's minibatch
+        for itn in range(n_iter):
+            random.shuffle(TRAIN_DATA)
+            batches = minibatch(TRAIN_DATA, size=sizes)
+            losses = {}
+            for batch in batches:
+                texts, annotations = zip(*batch)
+                nlp.update(texts, annotations, sgd=optimizer, drop=0.25, losses=losses)
+            print("Working on iteration: " + str(itn + 1) + '/' + str(n_iter) + ' with losses ', losses)
+
+    # test the trained model
+    test_text = "Emil blev født d. 17. august 1998, Mathias blev født 1995 19. maj."
+    doc = nlp(test_text)
+    print("Entities in '%s'" % test_text)
+    for ent in doc.ents:
+        print(ent.label_, ent.text)
+
+    # save model to output directory
+    if output_dir is not None:
+        output_dir = Path(output_dir)
+        if not output_dir.exists():
+            output_dir.mkdir()
+        nlp.meta["name"] = new_model_name  # rename model
+        nlp.to_disk(output_dir)
+        print("Saved model to", output_dir)
+
+        # test the saved model
+        print("Loading from", output_dir)
+        nlp2 = spacy.load(output_dir)
+        # Check the classes have loaded back consistently
+        assert nlp2.get_pipe("ner").move_names == move_names
+        doc2 = nlp2(test_text)
+        for ent in doc2.ents:
+            print(ent.label_, ent.text)
+
+
+if __name__ == "__main__":
+    plac.call(main)
