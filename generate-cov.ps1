9874438f7df69ca8e262650e3e00f7c571d81815
Zaph <35431127+Zaph-x@users.noreply.github.com>
2021-01-22 11:51:57 +0100

    Update generate-cov.ps1

--- generate-cov.ps1
+++ generate-cov.ps1
@@ -1,2 +1,2 @@
 #!/bin/bash
-pytest --cov-report term --cov=environment --cov=extractor --cov-report term-missing --cov=loader --cov=rdf -cov=turtleParser
+pytest --cov-report term --cov=environment --cov=extractor --cov-report term-missing --cov=loader --cov=rdf --cov=turtleParser
